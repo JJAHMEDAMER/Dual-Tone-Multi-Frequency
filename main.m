3c54da9744a75f8355bbe3260d56cddf4287af05
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-12-23 20:33:24 +0200

    plot and save phone tone

--- main.m
+++ main.m
@@ -10,5 +10,22 @@ function main()
     plot(tone_test)
     audiowrite('tone.wav',tone_test,fs);
 
+    phone_num = '01548754792';
     
+    % Guard Tone
+    time_sample_guard = 0:(1/fs):0.02;
+    guard_tone = tone(0, 0, time_sample_guard);
+
+    % Phone Tone
+    phone_num_tone = [];
+    for i = 1:length(phone_num)
+        [frq_low, frq_high] = sym2frq(phone_num(i));
+        ton = tone(frq_low, frq_high, time_sample);
+        phone_num_tone = [phone_num_tone guard_tone ton];
+    end
+    
+    figure()
+    plot(phone_num_tone)
+    audiowrite('phone_num_tone.wav',phone_num_tone,fs);
+
 end
